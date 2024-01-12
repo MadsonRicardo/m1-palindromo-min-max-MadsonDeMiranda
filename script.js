db8a7e4060c8f4e29a83eca007168f3cbc91aff6
Madson de Miranda <madson_miranda@hotmail.com.br>
2024-01-12 12:11:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,48 @@
-function isPalindrome(str){
-    /* Seu código aqui */
+function isPalindrome(str) {
+
+    const stringFormatada = str.toLowerCase().replace(/\s/g, '');
+    let stringRetornadaInverso = '';
+
+    for (let i = stringFormatada.length - 1; i >= 0; i--) {
+
+        stringRetornadaInverso = stringRetornadaInverso + stringFormatada[i];
+
+    }
+
+    if (stringRetornadaInverso === stringFormatada) {
+
+        return true;
+
+    } else {
+
+        return false;
+
+    }
+
 }
 
-function arrayMaxMin(arr){
-    /* Seu código aqui */
+// isPalindrome('A mala nada na lama')
+
+function arrayMaxMin(arr) {
+
+    let maiorNumero = arr[0];
+    let menorNumero = arr[0];
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if (maiorNumero <= arr[i]) {
+
+            maiorNumero = arr[i];
+
+        } else if (menorNumero >= arr[i]) {
+
+            menorNumero = arr[i];
+
+        }
+    }
+
+    return [menorNumero, maiorNumero];
 }
+
+
+// arrayMaxMin([5, 3, 15, 25, 50, 0, 2, 8, 1]);
